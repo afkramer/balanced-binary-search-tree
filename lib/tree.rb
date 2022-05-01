c09238b7d1106073d4338cf7d5a1d76df8fcc427
Adriana Kramer <adri.kramer@gmail.com>
2022-05-01 11:26:23 +0200

    Add Tree#build_tree method

--- lib/tree.rb
+++ lib/tree.rb
@@ -1,8 +1,24 @@
 # frozen_string_literal: true
 
+require_relative './node'
+
 # Tree class contains the functionality to build a balanced binary search tree
 class Tree
-  def initialize
-    @root = build_tree
+  def initialize(array)
+    @root = build_tree(array.sort.uniq, 0, array.sort.uniq.length - 1)
+  end
+
+  def build_tree(array, beg, last)
+    return nil if beg > last
+
+    mid = (beg + last) / 2
+    root = Node.new(array[mid])
+
+    root.left = build_tree(array, beg, mid - 1)
+    root.right = build_tree(array, mid + 1, last)
+
+    root
   end
 end
+
+tree = Tree.new([9, 8, 9, 1, 7, 3, 1, 9, 5, 2, 4])
