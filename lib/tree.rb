448e5a9b3435ee98f1dddc08dbbc268258fde314
Adriana Kramer <adri.kramer@gmail.com>
2022-05-05 17:58:46 +0200

    Add Tree#level_order_rec

--- lib/tree.rb
+++ lib/tree.rb
@@ -83,6 +83,42 @@ class Tree
     end
   end
 
+  def level_order
+    return root if root.nil?
+
+    to_traverse = [root]
+    values = []
+    until to_traverse.empty?
+      curr_node = to_traverse.shift
+      to_traverse << curr_node.left unless curr_node.left.nil?
+      to_traverse << curr_node.right unless curr_node.right.nil?
+      block_given? ? yield(curr_node) : values << curr_node.data
+    end
+    return values unless values.empty?
+  end
+
+  def level_order_rec(to_traverse = [root], values = [], &block)
+    node = to_traverse.shift
+    return node if node.nil?
+
+    to_traverse << node.left unless node.left.nil?
+    to_traverse << node.right unless node.right.nil?
+
+    block_given? ? yield(node) : values << node.data
+    level_order_rec(to_traverse, values, &block)
+    return values unless block_given?
+  end
+
+  # Nope --> this does a kind of depth first search
+  def level_order_rec_1(root, values = [])
+    return root if root.nil?
+
+    block_given? ? yield(root) : values << root.data
+    level_order_rec(root.left, values)
+    level_order_rec(root.right, values)
+    return values unless values.empty?
+  end
+
   # Method provided by student of TOP
   def pretty_print(node = @root, prefix = '', is_left = true)
     pretty_print(node.right, "#{prefix}#{is_left ? '│   ' : '    '}", false) if node.right
@@ -95,5 +131,9 @@ tree = Tree.new([90, 80, 90, 10, 70, 30, 10, 90, 50, 20, 40])
 tree.insert(tree.root, 60)
 tree.insert(tree.root, 5)
 tree.insert(tree.root, 25)
-tree.pretty_print
 tree.find(tree.root, 25)
+tree2 = Tree.new([36,81,50,30,20,85,82,75,34,32,85,81,50,40,70,60,82,81,65,70,32,36,75])
+tree2.insert(tree2.root, 42)
+tree2.insert(tree2.root, 90)
+# tree2.pretty_print
+p tree2.level_order_rec
